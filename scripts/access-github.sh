ef8254a5535ad97bef0458e8c05369227b1e75c7
natashaw <natashawijoyo@gmail.com>
2022-07-25 21:59:55 +0200

    sanitize and add bash script for gh notifications

---
+++ scripts/access-github.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Reviews and Mentions (sh)
+# @raycast.mode fullOutput
+
+export GH_TOKEN=REMOVED
+
+./github-notifications.rb
